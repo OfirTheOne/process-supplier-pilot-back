e4c479e16887f0bfe3c1e092f4e72e63a33d6a17
TheCodeSleyer <ofir3322@gmail.com>
2018-11-04 23:57:11 +0200

    set server

--- server/whitelist-orgins.ts
+++ server/whitelist-orgins.ts
@@ -1,4 +1,6 @@
 export const whiteOrgins: string[] = [
+    'https://secret-depths-77560.herokuapp.com/',
+    'https://secret-depths-77560.herokuapp.com',
     'http://localhost:4200',
     'http://localhost:4200/', 
     'localhost:4200/',
